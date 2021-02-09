62c5c8411b80f524f15c47b89cde210cb172884e
Axel SCHLOSSER <aschlosser@adista.fr>
2021-02-09 09:42:19 +0100

    Revert "Front : Register + Account + Login + EditPassword"
    This reverts commit 4ca1a5e88f9d22a1f8127257944b36e15780294d.

--- Front/admin/src/app/app-routing.module.ts
+++ Front/admin/src/app/app-routing.module.ts
@@ -1,18 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginClient } from './LoginClient/loginClient';
-import { RegisterClientComponent } from './registerClient/registerClient.component';
-import { StockComponent } from "./AdminStock/AdminStock.component";
-import { EditPasswordComponent } from "./editPassword/editPassword.component";
-import { MyAccountComponent } from "./myAccount/myAccount.component"
 
-const routes: Routes = [
-  {path:'', component:LoginClient},
-  {path:'register', component:RegisterClientComponent},
-  {path:'admin/stock', component:StockComponent},
-  {path:'editPassword', component:EditPasswordComponent},
-  {path:'myAccount', component:MyAccountComponent}
-];
+
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front/admin/src/app/app.module.ts
+++ Front/admin/src/app/app.module.ts
@@ -1,40 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { LoginClient } from './LoginClient/loginClient';
-import { RegisterClientComponent } from './registerClient/registerClient.component';
-import { StockComponent } from './AdminStock/AdminStock.component';
-import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule } from "@angular/material/table";
-import {MatPaginatorModule} from '@angular/material/paginator';
-import { EditPasswordComponent } from './editPassword/editPassword.component';
-import { MyAccountComponent } from './myAccount/myAccount.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FooterComponent,
-    HeaderComponent,
-    LoginClient,
-    RegisterClientComponent,
-    StockComponent,
-    EditPasswordComponent,
-    MyAccountComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    CommonModule,
-    BrowserAnimationsModule,
-    MatTableModule,
-    MatPaginatorModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
