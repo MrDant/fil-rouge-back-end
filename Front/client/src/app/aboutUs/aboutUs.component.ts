c1aa09f55019039e86bcdeab10228328c3316622
Axel Schlosser <axel.schlosser@gmail.com>
2021-02-28 17:59:35 +0100

    fix about us

--- Front/client/src/app/aboutUs/aboutUs.component.ts
+++ Front/client/src/app/aboutUs/aboutUs.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-aboutUs',
-  templateUrl: './aboutus.component.html',
+  templateUrl: './aboutUs.component.html',
   styleUrls: ['./aboutUs.component.scss']
 })
 export class AboutUsComponent implements OnInit {
