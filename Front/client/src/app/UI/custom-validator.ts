f7d65430eee40ac47d997c86d4bfba1691a0b2bb
MrDant <mael.dantuono@gmail.com>
2021-03-06 20:22:12 +0100

    delete front

--- Front/client/src/app/UI/custom-validator.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractControl, ValidationErrors } from "@angular/forms";
-
-export class CustomValidator {
-  static email(control: AbstractControl): ValidationErrors | null {
-    return null;
-  }
-}
